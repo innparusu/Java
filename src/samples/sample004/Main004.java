2e78bcb33b863baf2d4be7090209e9026fef1cef
innparusu <innparusu95@yahoo.co.jp>
2013-08-20 03:07:03 +0900

    add Main004.java

--- src/samples/sample004/Main004.java
+++ src/samples/sample004/Main004.java
@@ -6,10 +6,8 @@ public class Main004 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String str;
 		for(int i=1;i<=100;i++){
-			str = (i%15==0) ? "FIZZ BUZZ" : (i%3==0) ?  "FIZZ" : (i%5==0)? "BUZZ":"";
-			System.out.println(i+str);
+			System.out.println(i+":"+((i%15==0) ? "FIZZ BUZZ" : (i%3==0) ?  "FIZZ" : (i%5==0)? "BUZZ":""));
 		}
 	}
 }
