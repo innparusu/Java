06353d909d9a2988ddf0189a15b0260f4c69addc
innparusu <innparusu95@yahoo.co.jp>
2013-09-05 04:04:11 +0900

    add sample028

---
+++ src/samples/sample028/Class2.java
@@ -0,0 +1,10 @@
+package samples.sample028;
+
+import samples.sample027.Class1;
+
+public class Class2 extends Class1 {
+	Class2() {
+		super();
+		hugi = "huge";
+	}
+}
---
+++ src/samples/sample028/Main028.java
@@ -0,0 +1,13 @@
+package samples.sample028;
+
+import samples.sample027.Class1;
+
+
+public class Main028 {
+
+	public static void main(String[] args) {
+		Class1 hoge = new Class1();
+		System.out.println(hoge.hoge);
+	}
+
+}
