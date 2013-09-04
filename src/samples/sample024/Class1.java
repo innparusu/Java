cb987a1f81893f098b64547ed1b3db39657230e8
innparusu <innparusu95@yahoo.co.jp>
2013-09-05 00:51:00 +0900

    add sample024

---
+++ src/samples/sample024/Class1.java
@@ -0,0 +1,23 @@
+package samples.sample024;
+
+public class Class1 {
+	String anime1,anime2,anime3;
+	
+	Class1(String ani1,String ani2,String ani3) {
+		anime1 = ani1;
+		anime2 = ani2;
+		anime3 = ani3;
+	}
+
+	void printAnime1(){
+		System.out.println(anime1);
+	}
+
+	void printAnime2(){
+		System.out.println(anime2);
+	}
+
+	void printAnime3(){
+		System.out.println(anime3);
+	}
+}
---
+++ src/samples/sample024/Main024.java
@@ -0,0 +1,10 @@
+package samples.sample024;
+
+public class Main024 {
+	public static void main(String[] args){
+	Class1 hoge = new Class1("わたもて","れーるがん","カブトボーグ");
+	hoge.printAnime1();
+	hoge.printAnime2();
+	hoge.printAnime3();
+	}
+}
